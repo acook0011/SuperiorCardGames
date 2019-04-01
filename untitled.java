d88fefbfcc48f085175bbb062f6e8a388cf023d7
Makai <mromero0044@myduneland.org>
2019-04-01 11:26:49 -0500

    update untitled

--- untitled.java
+++ untitled.java
@@ -7,27 +7,5 @@
  */
 public class untitled
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class untitled
-     */
-    public untitled()
-    {
-        // initialise instance variables
-        x = 0;
-    }
-
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
-    }
+    
 }
