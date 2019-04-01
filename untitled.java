c815f6e75619e229abe21cd68ab37c9a61e5d09b
Makai <mromero0044@myduneland.org>
2019-04-01 12:30:57 -0500

    Update untitled

--- untitled.java
+++ untitled.java
@@ -1,11 +1,24 @@
 
 /**
- * Write a description of class untitled here.
+ * the
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @Makai Romero
+ * @0.0
  */
+
+import java.util.ArrayList;
+import java.util.Arrays;
 public class untitled
 {
-    
+    //Deck deck = new Deck();
+    //Card[] card = deck.deal(26);
+
+    public static void main (String[] args){
+        Deck deck = new Deck();
+        deck.shuffle();
+        Card[] card = deck.deal(52);
+        ArrayList<Card> curd = new ArrayList<Card>(Arrays.asList(card));
+        
+        System.out.println(curd);
+    }
 }
