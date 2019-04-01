751a702a7e22f7971b188fd59e4bb5ffc5a7f961
Avery Cook <acook0011@myduneland.org>
2019-04-01 11:31:23 -0500

    Jack is Black

--- Blackjack.java
+++ Blackjack.java
@@ -6,40 +6,20 @@ import java.util.*;
  * @aj
  * @blackjack
  * 
- * I might be doing this instead of Texas Hold'em for the sake of simplicity.
- * The checks are definitely proving to be more difficult to properly handle
- * than what they're worth.
- * 
  */
 public class Blackjack
 {
-    private Deck deck;
-    private ArrayList<Card> p1Hand;
-    private ArrayList<Card> p2Hand;
-    
-    public Blackjack(){
-        init();
-    }
-    
-    private void init(){
-        deck = new Deck();
-        deck.shuffle();
-        p1Hand = new ArrayList<Card>();
-        p2Hand = new ArrayList<Card>();
-    }
-    
-    public void dealHand(){
-        p1Hand.add(deck.deal());
-        p1Hand.add(deck.deal());
-        p2Hand.add(deck.deal());
-        p2Hand.add(deck.deal());
-        System.out.println(p1Hand);
-        System.out.println(p2Hand);
-    }
-    
-    // 'Get' methods
-    public void getHands(){
-        System.out.println(p1Hand);
-        System.out.println(p2Hand);
+    public static void Jack(){
+        Deck deck = new Deck();
+        ArrayList<Card> p1 = new ArrayList<Card>();
+        ArrayList<Card> comp = new ArrayList<Card>();
+        
+        p1.add(deck.deal());
+        p1.add(deck.deal());
+        comp.add(deck.deal());
+        comp.add(deck.deal());
+        System.out.println("Your hand: " + p1);
+        
+        
     }
 }
--- TestGames.java
+++ TestGames.java
@@ -8,14 +8,7 @@
 public class TestGames
 {
     public static void main(String [] args){
-        TexasHoldem Holdem = new TexasHoldem();
-        Holdem.dealHand();
-        Holdem.getHands();
-        Holdem.dealRiver();
-        Holdem.checkOdds();
-        
-        Blackjack Jack = new Blackjack();
-        Jack.dealHand();
+        Blackjack.Jack();
         
     }
 }
