ce08d0da80dbf03bbedfb557f34f15a51a9042be
Makai <mromero0044@myduneland.org>
2019-04-03 12:32:52 -0500

    Update War

--- ThisIsMyTester.java
+++ ThisIsMyTester.java
@@ -3,7 +3,6 @@
  * Temp Tester for War.
  *
  * @Makai Romero
- * @1.0
  */
 
 import java.util.ArrayList;
@@ -13,7 +12,8 @@ public class ThisIsMyTester
     public static void main (String[] args){
         War war = new War();
         ArrayList<String> Deck1 = new ArrayList<String>();
-        System.out.println(war.getDeck());
+        Deck1 = war.getDeck();
+        System.out.println("Here is your deck: \n" + Deck1.subList(0,25)); //The other half will be the opponent's.
         
     }
 }
