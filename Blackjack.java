e63943db011fd4341977199a045dee6d3c870a50
Avery Cook <acook0011@myduneland.org>
2019-04-12 13:05:33 -0500

    Jack is Super Black

--- Blackjack.java
+++ Blackjack.java
@@ -10,8 +10,6 @@ import java.util.*;
 public class Blackjack
 {
     private static Deck deck;
-    private static ArrayList<Card> p1;
-    private static ArrayList<Card> comp;
     private static ArrayList<ArrayList<Card>> p;
     private static int[] score;
     private static int[] cash;
@@ -20,9 +18,6 @@ public class Blackjack
     public static void Jack(){
         deck = new Deck();
         p = new ArrayList<ArrayList<Card>>(); // p.get(0) = Dealer
-        
-        p1 = new ArrayList<Card>();
-        comp = new ArrayList<Card>();
         Scanner reader = new Scanner(System.in);
         
         deck.shuffle();
@@ -41,118 +36,17 @@ public class Blackjack
                                "- " + p.get(i).get(0) + "\n" +
                                "- " + p.get(i).get(1) + "\n");
         }
-        
+        // Calculates Starting Score
+        for (int i = 0; i < players+1; i++){
+            score[i] = score(p.get(i));
+        }
         play(p, score, bust);
-        // OLD BELOW I still need to implement proper multiplayer
         
-        // Deals initial hands
-        deck.shuffle();
-        p1.add(deck.deal());
-        p1.add(deck.deal());
-        comp.add(deck.deal());
-        comp.add(deck.deal());
-        
-        
-        Boolean playing = true;
-        int score = score(p1);
-        System.out.println(score);
-        System.out.println("Dealer's visible hand: \n" + 
-                           "- " + comp.get(0));
-        if (score > 21){
-            System.out.println("Player has a Natural!");
-        }
-        
-        int count = 0;
-        while (playing){
-          // Displays current hand
-          score = score(p1);
-          System.out.println("Your current hand: ");
-          for (int i = 0; i < p1.size(); i++){
-              System.out.println("- " + p1.get(i));
-          }
-          System.out.println("Score: " + score(p1));
-          
-          // Checks if 21
-          if (score == 21){
-              playing = false;
-              System.out.println("You got 21!");
-          }
-          // Checks if over 21
-          else if (score > 21){
-              playing = false;
-              System.out.println("YOU B U S T E D");
-              System.out.println();
-          }
-          // Continues to Play
-          else {
-              System.out.println("Hit? [Y/N]");
-              Boolean valid = false;
-              while (!valid){
-                  String reply = reader.nextLine();
-                  System.out.println();
-                  if (reply.toUpperCase().equals("Y")){
-                      p1.add(deck.deal());
-                      valid = true;
-                  } else if (reply.toUpperCase().equals("N")){
-                      playing = false;
-                      valid = true;
-                  } else {
-                      System.out.println("Respond with Y or N.");
-                  }
-              }
-          }
-          
-          count++;
-        }
-        
-        // Computer/Dealer's turn
-        int scoreD = score(comp);
-        playing = true;
-        count = 0;
-        while (playing){
-          // Displays current hand
-          scoreD = score(comp);
-          System.out.println("Dealer's current hand: ");
-          for (int i = 0; i < comp.size(); i++){
-              System.out.println("- " + comp.get(i));
-          }
-          System.out.println("Score: " + score(comp));
-          
-          // Checks if 21
-          if (scoreD == 21){
-              playing = false;
-          }
-          // Checks if over 21
-          else if (scoreD > 21){
-              playing = false;
-          }
-          // Stops hitting
-          else if (scoreD >= 17){
-              System.out.println("Dealer stays.");
-              playing = false;
-          } 
-          // Continues playing
-          else{
-              comp.add(deck.deal());
-          }
-          
-          count++;
-        }
-        
-        System.out.println("\nThe Dealer has a score of " + scoreD + " with the hand: ");
-        for (int i = 0; i < comp.size(); i++){
-            System.out.println("- " + comp.get(i));
-        }
-        System.out.println(score(comp));
-        System.out.println("The Player has a score of " + score + " with the hand: ");
-        for (int i = 0; i < p1.size(); i++){
-            System.out.println("- " + p1.get(i));
-        }
-        System.out.println(score(p1));
 
     }
     
-    public static ArrayList<ArrayList<Card>> dealHand(ArrayList<ArrayList<Card>> p, int players){
+    public static void dealHand(ArrayList<ArrayList<Card>> p, 
+                                int players){
         // 0 = The Dealer
         for (int i = 0; i < players+1; i++){
             p.add(new ArrayList<Card>());
@@ -161,22 +55,26 @@ public class Blackjack
             p.get(i).add(deck.deal());
             p.get(i).add(deck.deal());
         }
-        return p;
     }
     
-    public static ArrayList<ArrayList<Card>> play(ArrayList<ArrayList<Card>> p, 
-                                                  int[] score, boolean[] bust){                                                      
+    public static void play(ArrayList<ArrayList<Card>> p, 
+                            int[] score, boolean[] bust){                                                      
         Scanner reader = new Scanner(System.in);
         Boolean[] stay = new Boolean[bust.length];
-        for (int i = 0; i < stay.length; i++){
-            stay[i] = false;
-        }
+       for (int i = 0; i < stay.length; i++){
+           if ((score[i] == 21) && (i != 0)){ //Checks for Naturals
+               System.out.println("! Player " + i + " has a Natural 21 !\n");
+               stay[i] = true;
+           } else {
+               stay[i] = false;
+           }
+       }
         Boolean playing = true;
         // Players
         while (playing){
          for (int x = 1; x < p.size(); x++){
           
-          if (!bust[x] || !stay[x]){
+          if (!bust[x] && !stay[x]){
            // Displays current hand
            System.out.println("~~Player " + x + "'s turn~~"); 
            score[x] = score(p.get(x));
@@ -217,9 +115,9 @@ public class Blackjack
             System.out.println();
            }
          }
-         int a = p.size(); // Amount of players who have not busted or stayed
+         int a = p.size()-1; // Amount of players who have not busted or stayed
          for (int j = 1; j < p.size(); j++){
-            if (!bust[j] || !stay[j]){
+            if (bust[j] || stay[j]){
                 a--;
             }
          }
@@ -227,7 +125,6 @@ public class Blackjack
             playing = false;
          }
        }
-       return p;
     }
     
     public static int score(ArrayList<Card> hand){
@@ -238,8 +135,8 @@ public class Blackjack
         
         if (score > 21){ // Checks if there are Aces
             int aceCount = 0;
-            for (int i = 0; i < p1.size(); i++){
-                if (score > 21 && p1.get(i).getRank() == 1){
+            for (int i = 0; i < hand.size(); i++){
+                if (score > 21 && hand.get(i).getRank() == 1){
                     score -= 10;   
                 }
             }
