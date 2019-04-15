b1847031f89fe89f63b0521eaa5ea0f615585e22
Avery Cook <acook0011@myduneland.org>
2019-04-15 12:30:45 -0500

    Jack is Black

--- TestJack.java
+++ TestJack.java
@@ -1,3 +1,4 @@
+import java.util.*;
 
 /**
  * Write a description of class TestJack here.
@@ -7,7 +8,11 @@
  */
 public class TestJack
 {
+    //public static int players;
+    
     public static void main(String [] args){
+        //Scanner reader = new Scanner(System.in);
+        //players = reader.nextInt();
         Blackjack.Jack();
         
     }
