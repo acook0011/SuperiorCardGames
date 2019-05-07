28e159b7180f2eba99c13f3f7231a1bbf3f41d45
Gavin <gdooley0096@myduneland.org>
2019-05-07 12:14:56 -0500

    afds

--- MaoTester.java
+++ MaoTester.java
@@ -4,6 +4,9 @@
 public class MaoTester
 {
     public static void main(String [] args){
-        Mao.playMao();
+        for(int i=0; i<21; i++){
+            int rule = Mao.randyRules(i%3+2);
+            System.out.println(rule+": "+Mao.stringRule(rule));
+        }
     }
 }
